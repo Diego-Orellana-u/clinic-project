5179da336a0c9b14087e403a58817e20594418b3
diego orellana <diego.orellana.git@gmail.com>
2024-06-18 13:32:22 -0400

    modified some files to remove icons

--- frontend/src/components/Dashboard/Navbar.jsx
+++ frontend/src/components/Dashboard/Navbar.jsx
@@ -1,37 +1,31 @@
-import search from '/img/search.svg'
-import setting from '/img/setting.svg'
-import moon from '/img/moon.svg'
-import MenuIcon from '../icons/MenuIcon'
+// import search from '/img/search.svg'
+// import setting from '/img/setting.svg'
+// import moon from '/img/moon.svg'
+import MenuIcon from "../icons/MenuIcon";
 
-
-export default function Navbar({ setActiveSidebar, activeSidebar}){
-
-  return(
-    <section className='h-[50px] flex items-center px-4 justify-between'>
-      <div className='flex'>
-        <div className='flex items-center gap-x-[.5rem]'>
-          <div> 
+export default function Navbar({ setActiveSidebar, activeSidebar }) {
+  return (
+    <section className="h-[50px] flex items-center px-4 justify-between">
+      <div className="flex">
+        <div className="flex items-center gap-x-[.5rem]">
+          <div>
             <MenuIcon onClick={() => setActiveSidebar(!activeSidebar)} />
           </div>
           <div>
-            <input 
-              type='text'
-              className='bg-[#4d5d68] h-[30px] w-[150px] min-[375px]:w-[200px] rounded-lg pl-3 pt-1 text-white outline-none'
-              placeholder='Buscar...'
+            <input
+              type="text"
+              className="bg-[#4d5d68] h-[30px] w-[150px] min-[375px]:w-[200px] rounded-lg pl-3 pt-1 text-white outline-none"
+              placeholder="Buscar..."
             />
           </div>
-          <img src={search} />
+          {/* <img src={search} /> */}
         </div>
       </div>
 
-      <div className='flex'>
-        <div>
-          <img src={moon} />
-        </div>
-        <div>
-          <img src={setting} />
-        </div>
+      <div className="flex">
+        <div>{/* <img src={moon} /> */}</div>
+        <div>{/* <img src={setting} /> */}</div>
       </div>
     </section>
-  )
+  );
 }
--- frontend/src/components/NoticiaCard.jsx
+++ frontend/src/components/NoticiaCard.jsx
@@ -1,14 +1,12 @@
-import { Link } from "react-router-dom"
-import bloodPresure from "../assets/bloodPresure.jpg"
-import arrow from '/img/arrow.svg'
+import { Link } from "react-router-dom";
+import bloodPresure from "../assets/bloodPresure.jpg";
+// import arrow from '/img/arrow.svg'
 
-export default function NoticiaCard(){
-
+export default function NoticiaCard() {
   const desc = `When stocking up on the necessities for life with a newborn—diapers, onesies, cute beanies and snuggly 
-  blankets—consider adding an important self-care item to your list: a home blood pressure cuff.`
+  blankets—consider adding an important self-care item to your list: a home blood pressure cuff.`;
 
-
-  return(
+  return (
     <Link>
       <div className="bg-[#4d5d68] h-[620px] w-[360px] rounded-3xl">
         <div className="rounded-xl overflow-hidden">
@@ -24,19 +22,17 @@ export default function NoticiaCard(){
             </div>
 
             <div>
-              <p>
-                {desc.substring(0,150) + '...'}
-              </p>
+              <p>{desc.substring(0, 150) + "..."}</p>
             </div>
 
             <div>
               <button className="h-[50px] w-[50px] rounded-full bg-[#fff] flex items-center justify-center">
-                <img src={arrow} />
+                {/* <img src={arrow} /> */}
               </button>
             </div>
           </div>
         </div>
       </div>
     </Link>
-  )
+  );
 }
